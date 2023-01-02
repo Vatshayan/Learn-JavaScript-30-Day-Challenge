70bd4586f5e7299f5218e62dcff27663b79d36f0
Shivam Vatshayan <vatshayanshiv1998@gmail.com>
2023-01-02 16:50:17 +0530

    Update 02-Day2-Jan2.js

--- 02-Day2-Jan2.js
+++ 02-Day2-Jan2.js
@@ -6,6 +6,8 @@ console.log('Woohoo! I love to code! #codecademy')
 console.log(20.49)
 
 
+
+// Numbers
 // On a new line write another console.log(). Inside the parentheses, take the current year and subtract 1969.
 console.log(2023-1969);
 
@@ -14,3 +16,8 @@ console.log(65/240);
 
 // Create one last console.log(). Inside the parentheses, multiply 0.2708 by 100.
 console.log(0.2708*100);
+
+
+// Strings 
+// Inside a console.log() statement, concatenate the two strings 'Hello' and 'World'.
+console.log('Hello'+'World');
