9db008d56988cd1f70aa06291c567843606d02f5
Shivam Vatshayan <vatshayanshiv1998@gmail.com>
2023-01-02 16:48:42 +0530

    Update 02-Day2-Jan2.js

--- 02-Day2-Jan2.js
+++ 02-Day2-Jan2.js
@@ -4,3 +4,13 @@ console.log('JavaScript');
 console.log(2011);
 console.log('Woohoo! I love to code! #codecademy')
 console.log(20.49)
+
+
+// On a new line write another console.log(). Inside the parentheses, take the current year and subtract 1969.
+console.log(2023-1969);
+
+// Create another console.log(). Inside the parentheses divide 65 by 240.
+console.log(65/240);
+
+// Create one last console.log(). Inside the parentheses, multiply 0.2708 by 100.
+console.log(0.2708*100);
