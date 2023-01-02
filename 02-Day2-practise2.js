0da66f4ec1181616139185a0b698e71b2f1a6662
Shivam Vatshayan <vatshayanshiv1998@gmail.com>
2023-01-02 17:04:19 +0530

    Update 02-Day2-practise2.js

--- 02-Day2-practise2.js
+++ 02-Day2-practise2.js
@@ -2,3 +2,19 @@
 
 // When you introduce a new piece of data into a JavaScript program, the browser saves it as an instance of the data type. All data types have access to specific properties that are passed down to each instance. For example, every string instance has a property called length that stores the number of characters in that string. You can retrieve property information by appending the string with a period and the property name:
  console.log('Hello'.length); // Prints 5
+
+
+// Methods 
+
+// On the first line, the .toUpperCase() method is called on the string instance 'hello'. The result is logged to the console. This method returns a string in all capital letters: 'HELLO'.
+console.log('hello'.toUpperCase()); // Prints HELLO
+
+// On the second line, the .startsWith() method is called on the string instance 'Hey'. This method also accepts the character 'H' as an input, or argument, between the parentheses. Since the string 'Hey' does start with the letter 'H', the method returns the boolean true.
+console.log('Hey'.startsWith('H')); //Prints True
+
+// In the second console.log() statement in app.js, we have a string ' Remove whitespace ' which has spaces before and after the words 'Remove whitespace'.
+
+// If we browse the JavaScript string documentation, we find several built-in string methods that each accomplish a different goal. The one method that seems ideal for us is .trim().
+console.log('    Remove whitespace   '.trim()); //Prints Remove whitespace
+
+
