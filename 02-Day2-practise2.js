8fe7ee54a949f897b997491274eb8df8994a02c4
Shivam Vatshayan <vatshayanshiv1998@gmail.com>
2023-01-02 16:57:09 +0530

    Create 02-Day2-practise2.js

---
+++ 02-Day2-practise2.js
@@ -0,0 +1,4 @@
+// Properties
+
+// When you introduce a new piece of data into a JavaScript program, the browser saves it as an instance of the data type. All data types have access to specific properties that are passed down to each instance. For example, every string instance has a property called length that stores the number of characters in that string. You can retrieve property information by appending the string with a period and the property name:
+ console.log('Hello'.length); // Prints 5
